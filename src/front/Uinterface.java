eb6a0a746005d268488800e95d455f55c93b8832
Lee-Rudy <120602756+Lee-Rudy@users.noreply.github.com>
2025-11-16 23:21:44 +0400

    update uinterface

--- src/front/Uinterface.java
+++ src/front/Uinterface.java
@@ -12,7 +12,7 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 
-import back.Calcul;
+import calcul.Calcul;
 
 public class Uinterface extends JFrame 
 {
